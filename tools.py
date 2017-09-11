dc05b077fe6ca6b324e7fe25532080cf8f9730e7
Aleks Lambreca <31127654+pynetscript@users.noreply.github.com>
2017-09-11 17:58:42 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,14 +1,14 @@
 from __future__ import absolute_import, division, print_function
-# "sudo pip install colorama" is required to download the Python2 package.
-# "sudo pip3 install colorama" is required to download the PYthon3 package.
+# "sudo pip install colorama" is required to download the Py2 package.
+# "sudo pip3 install colorama" is required to download the Py3 package.
 from colorama import init, Fore, Style
 
 
-### Getpass function asks for password but doesn't show it in screen.
+# Getpass function asks for password but doesn't show it in screen.
 from getpass import getpass
 
 
-### Custom function to get input that is compatible with Python 2 and 3.
+# Custom function to get input that is compatible with Py2 & 3.
 def get_input(prompt=''):
     try:
         line = raw_input(prompt)
@@ -17,7 +17,7 @@ def get_input(prompt=''):
     return line
 
 
-### Prompts for, and returns a username and password.
+# Prompts for, and returns a username and password.
 def get_credentials():
     print(Fore.YELLOW + '='*79 + Style.RESET_ALL)
     username = get_input('Username: ')
