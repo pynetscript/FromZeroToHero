db7cee97d9119a776b906eb45b3ee1ab6d2719c8
Aleks Lambreca <31127654+pynetscript@users.noreply.github.com>
2018-04-15 13:07:31 +0300

    Update cmdrunner.py

--- cmdrunner.py
+++ cmdrunner.py
@@ -116,7 +116,7 @@ start_time = start_timestamp.strftime('%d/%m/%Y %H:%M:%S')
 
 
 # Progress Bar
-widgets = [' '*29, '\n',
+widgets = ['\n',
            Percentage(), ' ', Bar(marker='#', left='[', right=']'),
            ' ', '[',SimpleProgress(),']',' ' '[', ETA(),']', '\n']
 
